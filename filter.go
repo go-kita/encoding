3a8231970c7cae2ace525e7a5fa6380857473296
dowenliu-xyz <hawkdowen@hotmail.com>
2021-06-28 00:10:21 +0800

    feat: encoding filter

---
+++ filter.go
@@ -0,0 +1,110 @@
+package encoding
+
+import (
+	"context"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/ianaindex"
+)
+
+// FilterFunc is a function which handle binary data.
+// Possible tasks are:
+//   - Textual content charset encoding / decoding.
+//   - Compression / Decompression
+//   - Encryption / Decryption.
+//   - Signing / Verifying.
+//   - ...
+type FilterFunc func(pre []byte) ([]byte, error)
+
+type filter struct {
+	fn          FilterFunc
+	marshaler   Marshaler
+	unmarshaler Unmarshaler
+}
+
+func (f *filter) Marshal(ctx context.Context, v interface{}) ([]byte, error) {
+	data, err := f.marshaler.Marshal(ctx, v)
+	if err != nil {
+		return nil, err
+	}
+	return f.fn(data)
+}
+
+func (f *filter) Unmarshal(ctx context.Context, data []byte, v interface{}) error {
+	data, err := f.fn(data)
+	if err != nil {
+		return err
+	}
+	return f.unmarshaler.Unmarshal(ctx, data, v)
+}
+
+// FilterMarshaler decorates a marshaler with FilterFunc. The binary data the
+// decorated Marshaler produced will be reprocessed by the FilterFunc.
+func FilterMarshaler(marshaler Marshaler, filterFunc ...FilterFunc) Marshaler {
+	for _, fn := range filterFunc {
+		marshaler = &filter{
+			fn:        fn,
+			marshaler: marshaler,
+		}
+	}
+	return marshaler
+}
+
+// FilterUnmarshaler decorates a unmarshaler with FilterFunc. The binary data the
+// decorated Unmarshaler to produce will be processed by the FilterFunc first.
+func FilterUnmarshaler(unmarshaler Unmarshaler, filterFunc ...FilterFunc) Unmarshaler {
+	for _, fn := range filterFunc {
+		unmarshaler = &filter{
+			fn:          fn,
+			unmarshaler: unmarshaler,
+		}
+	}
+	return unmarshaler
+}
+
+// EncodeWith produces a FilterFunc which re-encodes the binary data to specific encoding.
+// If the encoding provided is nil, the returned FilterFunc just keep the origin binary data.
+func EncodeWith(encoding encoding.Encoding) FilterFunc {
+	return func(pre []byte) ([]byte, error) {
+		if encoding == nil {
+			return pre, nil
+		}
+		return encoding.NewEncoder().Bytes(pre)
+	}
+}
+
+// EncodeWithCharset produces a FilterFunc which re-encodes the binary data to
+// encoding of specific name. If the charset / encoding is not supported by runtime
+// platform, the produced FilterFunc won't encode the data and return a non-nil error.
+func EncodeWithCharset(name string) FilterFunc {
+	return func(pre []byte) ([]byte, error) {
+		e, err := ianaindex.IANA.Encoding(name)
+		if err != nil {
+			return nil, err
+		}
+		return e.NewEncoder().Bytes(pre)
+	}
+}
+
+// DecodeWith produces a FilterFunc which decodes the binary data from specific encoding.
+// If the encoding provided is nil, the returned FilterFunc just keep the origin binary data.
+func DecodeWith(encoding encoding.Encoding) FilterFunc {
+	return func(pre []byte) ([]byte, error) {
+		if encoding == nil {
+			return pre, nil
+		}
+		return encoding.NewDecoder().Bytes(pre)
+	}
+}
+
+// DecodingWithCharset produces a FilterFunc which decodes the binary data from
+// encoding of specific name. If the charset / encoding is not supported by runtime
+// platform, the produced FilterFunc won't decode the data and return a non-nil error.
+func DecodingWithCharset(name string) FilterFunc {
+	return func(pre []byte) ([]byte, error) {
+		e, err := ianaindex.IANA.Encoding(name)
+		if err != nil {
+			return nil, err
+		}
+		return e.NewDecoder().Bytes(pre)
+	}
+}
---
+++ filter_test.go
@@ -0,0 +1,188 @@
+package encoding
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"golang.org/x/text/encoding/ianaindex"
+	"testing"
+)
+
+var _ Marshaler = (*errMarshaler)(nil)
+
+type errMarshaler struct {
+}
+
+func (e *errMarshaler) Marshal(_ context.Context, _ interface{}) ([]byte, error) {
+	return nil, errors.New("encoding: test error")
+}
+
+func TestFilter_Marshal(t *testing.T) {
+	stars := []byte("***")
+	f := &filter{
+		fn: func(pre []byte) ([]byte, error) {
+			return stars, nil
+		},
+		marshaler: &nopMarshaler{},
+	}
+	data, err := f.Marshal(context.Background(), "abc")
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+	if !bytes.Equal(data, stars) {
+		t.Errorf("expect %s, got %s", string(stars), string(data))
+	}
+	f.marshaler = &errMarshaler{}
+	_, err = f.Marshal(context.Background(), "abc")
+	if err == nil {
+		t.Errorf("expect an error, got nil")
+	}
+}
+
+func TestFilter_Unmarshal(t *testing.T) {
+	starts := []byte("***")
+	f := &filter{
+		fn: func(pre []byte) ([]byte, error) {
+			return starts, nil
+		},
+		unmarshaler: &nopUnmarshaler{},
+	}
+	str := "xyz"
+	err := f.Unmarshal(context.Background(), []byte("abc"), &str)
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+	if str != "xyz" {
+		t.Errorf("expect xyz, got %s", str)
+	}
+	f.fn = func(pre []byte) ([]byte, error) {
+		return nil, errors.New("encoding: test error")
+	}
+	err = f.Unmarshal(context.Background(), []byte("abc"), &str)
+	if err == nil {
+		t.Errorf("expect an error, got nil")
+	}
+}
+
+func TestFilterMarshaler(t *testing.T) {
+	marshaler := FilterMarshaler(nopMarshaler{},
+		func(pre []byte) ([]byte, error) {
+			return []byte("***"), nil
+		},
+		func(pre []byte) ([]byte, error) {
+			return nil, errors.New("encoding: test error")
+		},
+	)
+	if marshaler == nil {
+		t.Errorf("expect a marshaler, got nil")
+	}
+}
+
+func TestFilterUnmarshaler(t *testing.T) {
+	unmarshaler := FilterUnmarshaler(nopUnmarshaler{},
+		func(pre []byte) ([]byte, error) {
+			return []byte("***"), nil
+		},
+		func(pre []byte) ([]byte, error) {
+			return nil, errors.New("encoding: test error")
+		},
+	)
+	if unmarshaler == nil {
+		t.Errorf("expect an unmarshaler, got nil")
+	}
+}
+
+func TestEncodeWith(t *testing.T) {
+	encoding, _ := ianaindex.IANA.Encoding("ISO-8859-1")
+	filterFunc := EncodeWith(encoding)
+	_, err := filterFunc([]byte("中文"))
+	if err == nil {
+		t.Errorf("expect an error, got nil")
+	}
+	encoding, _ = ianaindex.IANA.Encoding("GBK")
+	filterFunc = EncodeWith(encoding)
+	data, err := filterFunc([]byte("中文"))
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+	expect := []byte{0xD6, 0xD0, 0xCE, 0xC4}
+	if !bytes.Equal(data, expect) {
+		t.Errorf("expect %#X, got %#X", expect, data)
+	}
+	filterFunc = EncodeWith(nil)
+	data, err = filterFunc([]byte("中文"))
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+	expect = []byte{0xE4, 0xB8, 0xAD, 0xE6, 0x96, 0x87}
+	if !bytes.Equal(data, expect) {
+		t.Errorf("expect %#X, got %#X", expect, data)
+	}
+}
+
+func TestEncodeWithCharset(t *testing.T) {
+	filterFunc := EncodeWithCharset("GBK")
+	data, err := filterFunc([]byte("中文"))
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+	expect := []byte{0xD6, 0xD0, 0xCE, 0xC4}
+	if !bytes.Equal(data, expect) {
+		t.Errorf("expect %U, got %U", expect, data)
+	}
+	filterFunc = EncodeWithCharset("My-Fake")
+	_, err = filterFunc([]byte("中文"))
+	if err == nil {
+		t.Errorf("expect an error, got nil")
+	}
+}
+
+func TestDecodeWith(t *testing.T) {
+	encoding, _ := ianaindex.IANA.Encoding("ISO-8859-1")
+	filterFunc := DecodeWith(encoding)
+	gbkData := []byte{0xD6, 0xD0, 0xCE, 0xC4}
+	data, err := filterFunc(gbkData)
+	if err != nil {
+		t.Errorf("expect an error, got nil")
+	}
+	expect := []byte{0xC3, 0x96, 0xC3, 0x90, 0xC3, 0x8E, 0xC3, 0x84}
+	if !bytes.Equal(data, expect) {
+		t.Errorf("expect %#X, got %#X", expect, data)
+	}
+	encoding, _ = ianaindex.IANA.Encoding("GBK")
+	filterFunc = DecodeWith(encoding)
+	data, err = filterFunc(gbkData)
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+	expect = []byte{0xE4, 0xB8, 0xAD, 0xE6, 0x96, 0x87}
+	if !bytes.Equal(data, expect) {
+		t.Errorf("expect %#2X, got %#2X", expect, data)
+	}
+	filterFunc = DecodeWith(nil)
+	data, err = filterFunc(gbkData)
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+	if !bytes.Equal(data, gbkData) {
+		t.Errorf("expect %#X, got %#X", gbkData, data)
+	}
+}
+
+func TestDecodingWithCharset(t *testing.T) {
+	filterFunc := DecodingWithCharset("GBK")
+	gbkData := []byte{0xD6, 0xD0, 0xCE, 0xC4}
+	data, err := filterFunc(gbkData)
+	if err != nil {
+		t.Errorf("expect nil, got %v", err)
+	}
+	expect := []byte("中文")
+	if !bytes.Equal(data, expect) {
+		t.Errorf("expect %#X, got %#X", expect, data)
+	}
+	filterFunc = DecodingWithCharset("MY-Fake")
+	_, err = filterFunc(gbkData)
+	if err == nil {
+		t.Errorf("expect an error, got nil")
+	}
+}
